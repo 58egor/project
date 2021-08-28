3de8400853c97035084381202ab44b7b943a941a
58egor <68480726+58egor@users.noreply.github.com>
2021-08-29 00:54:15 +0300

    was added cut script,restart and rotation for camera

---
+++ project/Assets/Buttons.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RayFire;
+
+public class Buttons : MonoBehaviour
+{
+    public RayfireRigid cube;
+    
+    // Start is called before the first frame update
+public void Restart()
+    {
+        GameObject parts = GameObject.Find("Cube_root");
+        if (parts != null)
+        {
+            Destroy(parts);
+        }
+        cube.ResetRigid();
+    }
+    
+}
